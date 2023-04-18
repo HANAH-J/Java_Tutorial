58a583d7cfa23fd6d1fbe555e80e1d35d250221b
HANA-J <hana21k@naver.com>
2023-04-18 20:24:27 +0900

    Feat: Chapter1 start - 01_Hello

---
+++ chap_01/start01/Hello.java
@@ -0,0 +1,14 @@
+package start01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+//		주석입니다.(ctrl+/)
+		System.out.println("eclipse Hello world");
+		//명령문 통으로 주석(ctrl+shift+/), 통으로 주석해제(ctrl+shift+/)
+		  System.out.print("eclipse "); System.out.print("Hello ");
+		  System.out.print("world");
+		 
+	}
+
+}
